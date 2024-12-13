d5548482d701bdc7237310c3566d870d1e9216c1
Ming Yan, Lin <xphoenixx32@gmail.com>
2024-12-13 10:33:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ st.set_page_config(
     page_title = 'Dashboard for EDA',
     layout = 'wide',
 )
+st.title('Dashboard for EDA')
+
+tab1, tab2, tab3 = st.tabs(['Data Summary', 'Filter Data Viewer', 'Interactive Dashbaord'])
 
 uploaded_file = st.file_uploader(
         'Choose a CSV file', 
@@ -16,42 +19,34 @@ uploaded_file = st.file_uploader(
 
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
-
-    st.subheader('Data Summary')
-    st.dataframe(df)
-    data_summary = df.describe([.25,.75,.9,.95])
-    data_types = df.dtypes.to_frame('Types')
-    st.write(data_summary, data_types.sort_values('Types'))
     
-    st.divider()
+    with tab1:
+        # st.subheader('Data Summary')
+        st.dataframe(df)
+        st.write(df.describe([.25,.75,.9,.95]))
+        data_types = df.dtypes.to_frame('Types')
+        st.write(data_types.sort_values('Types'))
+        
+        st.divider()
+    with tab2:
+        # st.subheader('Filter Data')
+        columns = df.columns.tolist()
+        selected_column = st.selectbox('Select column to filter by', columns)
+        unique_values = df[selected_column].unique()
+        selected_value = st.selectbox('Select value', unique_values)
 
-    st.subheader('Interactive Dashboard')
+        filtered_df = df[df[selected_column] == selected_value]
+        st.write(filtered_df)
 
-    @st.cache_resource
-    def get_pyg_renderer() -> 'StreamlitRenderer':
-        return StreamlitRenderer(df, spec = './gw_config.json', spec_io_mode = 'rw')
+        st.divider()
+    with tab3:
+        @st.cache_resource
+        def get_pyg_renderer() -> 'StreamlitRenderer':
+            return StreamlitRenderer(df, spec = './gw_config.json', spec_io_mode = 'rw')
 
-    renderer = get_pyg_renderer()
-    renderer.explorer()
+        renderer = get_pyg_renderer()
+        renderer.explorer()
 
-    st.divider()
-
-    st.subheader('Filter Data Viewer')
-    columns = df.columns.tolist()
-    selected_column = st.selectbox('Select column to filter by', columns)
-    unique_values = df[selected_column].unique()
-    selected_value = st.selectbox('Select value', unique_values)
-
-    filtered_df = df[df[selected_column] == selected_value]
-    st.write(filtered_df)
-
-    # st.subheader('Scatter Plot for Filtered Data')
-    # x_column = st.selectbox('Select X-axis column', columns)
-    # y_column = st.selectbox('Select Y-axis column', columns)
-
-    # if st.button('Generate Plot'):
-    #     st.scatter_chart(filtered_df.set_index(x_column)[y_column])
-    
-    # st.divider()
+        st.divider()
 else:
     st.write('Press "Browse Files" to Upload Data')
